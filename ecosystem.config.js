07c08157a5053d3270f3f4377404959af98f7281
Nayan Khedkar <khedkarnayan08@gmail.com>
2025-12-27 21:18:43 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      name: 'giftshop-api',
-      script: 'dist/src/main.js',
-      exec_mode: 'cluster',
-      instances: 'max',
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env_production: {
-        NODE_ENV: 'production',
-      },
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      error_file: 'logs/err.log',
-      out_file: 'logs/out.log',
-      merge_logs: true,
-    },
-  ],
+    apps: [
+        {
+            name: 'giftshop-api',
+            script: 'dist/main.js',
+            exec_mode: 'cluster',
+            instances: 'max',
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env_production: {
+                NODE_ENV: 'production',
+            },
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            error_file: 'logs/err.log',
+            out_file: 'logs/out.log',
+            merge_logs: true,
+        },
+    ],
 };
